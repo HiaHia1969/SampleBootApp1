68ed64c679de2d541d044b22a205f7f9ac9ffb1c
int.noguchi101127 <int.noguchi101127@gmail.com>
2023-12-28 22:22:37 +0900

    初期のコミット

---
+++ src/main/java/com/example/sample1app/SampleBootApp1Application.java
@@ -0,0 +1,121 @@
+package com.example.sample1app;
+
+import org.springframework.boot.Banner.Mode;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/* -------デフォルトのアプリケーション-------------*/
+//@SpringBootApplication
+//public class SampleBootApp1Application {
+//
+//	public static void main(String[] args) {
+//		//SpringApplicationクラスはSpringBootアプリケーションとしての基本的機能がまとめられている。
+//		//		runはアプリケーションを起動するためのメソッド
+//		//		引数には実行するクラスのClassインスタンスとパラメータとして渡すデータ
+//		//		いまのところ、＠SpringBootApplicationを付けたクラスをそのまま記述する。
+//
+//		//SpringApplicationインスタンスを生成
+//		//引数として、アプリケーションとして実行するクラスを選択
+//		//つまり、SpringBootApplicationアノテーションを付けたクラスのうち、実行したいクラスのClassインスタンスを引数として渡す
+//		//今回はSampleBootApp1ApplicationのClassインスタンスを引数として渡す
+//		SpringApplication app = new SpringApplication(SampleBootApp1Application.class);
+//		//setBannerModeをオフに設定することで、SprinApplicationの起動時の処理をカスタマイズできる。
+//		app.setBannerMode(Mode.OFF);
+//		app.run(args);
+//		//SpringApplication.run(SampleBootApp1Application.class, args);
+//	}
+//
+//}
+
+/* -------デフォルトのアプリケーション-------------*/
+@SpringBootApplication
+@RestController
+public class SampleBootApp1Application {
+	//	public class SampleBootApp1Application implements ApplicationRunner {
+
+	//	public static void main(String[] args) {
+	//		//SpringApplicationクラスはSpringBootアプリケーションとしての基本的機能がまとめられている。
+	//		//		runはアプリケーションを起動するためのメソッド
+	//		//		引数には実行するクラスのClassインスタンスとパラメータとして渡すデータ
+	//		//		いまのところ、＠SpringBootApplicationを付けたクラスをそのまま記述する。
+	//
+	//		//SpringApplicationインスタンスを生成
+	//		//引数として、アプリケーションとして実行するクラスを選択
+	//		//つまり、SpringBootApplicationアノテーションを付けたクラスのうち、実行したいクラスのClassインスタンスを引数として渡す
+	//		//今回はSampleBootApp1ApplicationのClassインスタンスを引数として渡す
+	//		SpringApplication app = new SpringApplication(SampleBootApp1Application.class);
+	//		//setBannerModeをオフに設定することで、SprinApplicationの起動時の処理をカスタマイズできる。
+	//		app.setBannerMode(Mode.OFF);
+	//		app.setHeadless(false);
+	//		app.run(args);
+	//		//SpringApplication.run(SampleBootApp1Application.class, args);
+	//	}
+
+	/* -------CommandLineRunnerでアプリケーションプログラム作成-------------*/
+	//	@Override
+	//	public void run(String[] args) {
+	//		System.out.println("+-------------------+");
+	//		System.out.println("| this is CommandLine Runner proguram. |");
+	//		System.out.println("+-------------------+");
+	//		System.out.println("[" + String.join(", ", args) + "]");
+	//	}
+
+	/* -------ApplicationRunnerでアプリケーションプログラム作成-------------*/
+	//	@Override
+	//	public void run(ApplicationArguments args) {
+	//		System.out.println("+-------------------+");
+	//		System.out.println("| this is Application Runner program. |");
+	//		System.out.println("+-------------------+");
+	//		System.out.println(args.getOptionNames());
+	//		System.out.println(args.getOptionValues("aaa"));
+	//		System.out.println(args.getNonOptionArgs());
+	//		System.out.println(Arrays.asList(args.getSourceArgs()));
+	//	}
+
+	/* -------UIアプリケーションの実行-------------*/
+	//	@Override
+	//	public void run(ApplicationArguments args) {
+	//		JFrame frame = new JFrame("Spring Boot Swing App");
+	//		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	//		frame.setSize(300, 200);
+	//		frame.add(new JLabel("Spring Boot Application."));
+	//		frame.setVisible(true);
+	//	}
+
+	/* -------RESTControllerの実装例-------------*/
+	public static void main(String[] args) {
+		//		SpringApplication.run(SampleBootApp1Application.class, args);
+
+		SpringApplication app = new SpringApplication(SampleBootApp1Application.class);
+		app.setBannerMode(Mode.OFF);
+		app.run(args);
+
+	}
+
+	@RequestMapping("/")
+	public String index() {
+		return "Hello, My Spring Boot 3!!!";
+	}
+
+	/* -------RESTControllerの実装例2-------------*/
+	String[][] data = {
+			{ "noname", "no email address", "0" },
+			{ "taro", "taro@exam.com", "39" },
+			{ "hanako", "hanako@exam.com", "28" },
+			{ "jiro", "jiro@exam.com", "17" },
+
+	};
+
+	@RequestMapping("/{num}")
+	public String index(@PathVariable int num) {
+		int n = num < 0 ? 0 : num >= data.length ? 0 : num;
+		String[] item = data[n];
+		String msg = "ID=%s, {name: %s, mail: %s, age: %s}";
+		return String.format(msg, num, item[0], item[1], item[2]);
+
+	}
+
+}
