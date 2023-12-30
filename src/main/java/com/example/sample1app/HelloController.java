47f407eed7220dce56547f5de4aef7446203e2a2
int.noguchi101127 <int.noguchi101127@gmail.com>
2023-12-30 11:38:17 +0900

    コントローラー側にパラメータを用意し、階乗結果を表示

--- src/main/java/com/example/sample1app/HelloController.java
+++ src/main/java/com/example/sample1app/HelloController.java
@@ -2,6 +2,7 @@ package com.example.sample1app;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -19,5 +20,20 @@ public class HelloController {
 		model.addAttribute("msg",
 				"buttonのページです");
 		return "button";
+	}
+
+	@RequestMapping("/{num}")
+	public String num(@PathVariable int num, Model model) {
+		int res = 0;
+		//		for (int i = 1; i <= num; i++) {
+		//			res += i;
+		//		}
+		while (res < num) {
+			res++;
+		}
+
+		model.addAttribute("msg",
+				"total: " + res);
+		return "num";
 	}
 }
