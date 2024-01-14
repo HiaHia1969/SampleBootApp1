90df5b12e04805060e6c8b5d35542245250fbf3e
int.noguchi101127 <int.noguchi101127@gmail.com>
2024-01-14 18:38:48 +0900

    HelloController.javaとindex.htmlの修正

--- src/main/java/com/example/sample1app/HelloController.java
+++ src/main/java/com/example/sample1app/HelloController.java
@@ -4,17 +4,26 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloController {
 
-	@RequestMapping("/")
-	public String index(Model model) {
-		model.addAttribute("msg",
-				"これはコントローラーに用意したメッセージです");
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public ModelAndView index(ModelAndView mav) {
+		mav.addObject("msg", "e-mailを記入してください。");
+		mav.setViewName("index");
+		return mav;
+	}
 
-		return "index";
+	@RequestMapping(value = "/", method = RequestMethod.POST)
+	public ModelAndView form(@RequestParam("text1") String str, ModelAndView mav) {
+		mav.addObject("msg", "あなたのメールアドレスは「" + str + "」ですね。");
+		mav.addObject("value", str);
+		mav.setViewName("index");
+		return mav;
 	}
 
 	@RequestMapping("/button")
