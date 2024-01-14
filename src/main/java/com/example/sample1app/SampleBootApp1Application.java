479da9d3813e26233a03061948621c5db6885dc9
int.noguchi101127 <int.noguchi101127@gmail.com>
2024-01-14 19:03:24 +0900

    dataの中身をString型として表示するように変更

--- src/main/java/com/example/sample1app/SampleBootApp1Application.java
+++ src/main/java/com/example/sample1app/SampleBootApp1Application.java
@@ -4,6 +4,7 @@ import org.springframework.boot.Banner.Mode;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -49,6 +50,30 @@ public class SampleBootApp1Application {
 				</html>
 				""";
 		return context;
+	}
+
+	/**
+	 * URLに数字を付け、数字が0以上かつdata配列の長さ以下であればそのdataのnum番目のオブジェクトを返す。
+	 *
+	 * @param num 取り出したい番号
+	 * @return data[num] dataに格納されているDataObject型を返す
+	 */
+	@RequestMapping("/{num}")
+	public DataObject num(@PathVariable int num) {
+		int n = num < 0 ? 0 : (num >= data.length ? 0 : num);
+		return data[num];
+	}
+
+	@RequestMapping("/person/{num}")
+	public String person(@PathVariable int num) {
+		int n = num < 0 ? 0 : (num >= data.length ? 0 : num);
+		DataObject person = data[num];
+		String str = """
+				名前：　%s \n
+				Mail　%s \n
+				年齢　%d
+				""";
+		return String.format(str, person.getName(), person.getMail(), person.getAge());
 	}
 }
 
