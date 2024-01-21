a280fe395f9f53b5e3ef59495468da6d7c5772ee
int.noguchi101127 <int.noguchi101127@gmail.com>
2024-01-21 16:47:19 +0900

    条件分岐と繰り返し処理を実装

--- src/main/java/com/example/sample1app/HelloController.java
+++ src/main/java/com/example/sample1app/HelloController.java
@@ -1,11 +1,16 @@
 package com.example.sample1app;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloController {
+
+	private boolean flag = false;
 
 	@GetMapping("/")
 	public ModelAndView index(ModelAndView mav) {
@@ -20,6 +25,43 @@ public class HelloController {
 		mav.setViewName("index");
 		mav.addObject("msg", "メッセージだよ");
 		mav.addObject("msg2", htmlMsg);
+		return mav;
+	}
+
+	@GetMapping("/condition")
+	public ModelAndView condition(ModelAndView mav) {
+
+		mav.setViewName("/condition");
+		flag = !flag;
+		mav.addObject("flag", flag);
+		mav.addObject("msg", "メッセージを表示します");
+		return mav;
+	}
+
+	@GetMapping("/repeat")
+	public ModelAndView repeat(ModelAndView mav) {
+		mav.setViewName("/repeat");
+
+		MyData[] datas = new MyData[] {
+				new MyData("aoi", 25),
+				new MyData("hinata", 24),
+				new MyData("yuna", 22),
+				new MyData("kento", 16)
+		};
+
+		List<String> arrayList = new ArrayList<String>() {
+			{
+				add("One");
+				add("Twe");
+				add("Three");
+				add("Four");
+				add("Five");
+			}
+		};
+
+		mav.addObject("data", arrayList);
+		mav.addObject("data2", datas);
+		mav.addObject("msg", "dataに格納されている要素を表示します");
 		return mav;
 	}
 }
---
+++ src/main/java/com/example/sample1app/MyData.java
@@ -0,0 +1,9 @@
+package com.example.sample1app;
+
+import lombok.Data;
+
+@Data
+public class MyData {
+	private final String name;
+	private final int age;
+}
